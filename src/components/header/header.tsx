0db0e474cb6a809bd907d13c4e9b8efab9abd5a8
Brayan Zarabanda <brayan.zarabanda.g@gmail.com>
2026-06-10 22:45:27 -0500

    feat: agregar componentes y conexiones de  Acerca de mí, Contacto, Inicio, Servicios y Equipo -  Componente de header agregado con nav y e iconos de redes con enlaces a redes - Estilos de header implementados segun guía.

---
+++ src/components/header/header.tsx
@@ -0,0 +1,67 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { NavLink } from 'react-router-dom'
+import { socialLinks } from './header.data'
+import logoImage from '../../assets/logo_referencia_legal.png'
+import './header.css'
+
+const navItems = [
+    { label: 'Inicio', to: '/' },
+    { label: 'Servicios', to: '/services' },
+    { label: 'Equipo', to: '/team' },
+    { label: 'Quiénes somos', to: '/about-me' },
+    { label: 'Contáctenos', to: '/contact' },
+]
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <NavLink className="header-logo-link" to="/">
+                    <img
+                        alt="Referencia Legal"
+                        className="header-logo"
+                        src={logoImage}
+                    />
+                </NavLink>
+
+                <div className="header-navigation">
+                    <div className="header-contact-row">                            <div className="header-social-links">
+                            <ul className="header-social-list">
+                                {socialLinks.map((socialLink) => (
+                                    <li className="header-social-item" key={socialLink.label}>
+                                        <a
+                                            className="header-social-link"
+                                            href={socialLink.href}
+                                            rel="noreferrer"
+                                            target="_blank"
+                                        >
+                                            <FontAwesomeIcon icon={socialLink.icon} />
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <nav className="navbar">
+                        <ul className="navbar-list">
+                            {navItems.map((navItem) => (
+                                <li className="navbar-item" key={navItem.to}>
+                                    <NavLink
+                                        className={({ isActive }) => 
+                                            `navbar-link${isActive ? ' navbar-link-active' : ''}`
+                                        }
+                                        end={navItem.to === '/'}
+                                        to={navItem.to}
+                                    >
+                                        {navItem.label}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
