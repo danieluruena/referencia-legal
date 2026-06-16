7b4fd3807dd8b6153015ebbb682fe4b5d003519d
Brayan Zarabanda <brayan.zarabanda.g@gmail.com>
2026-06-15 21:26:50 -0500

    Organizing HTML structure - team

--- src/components/team/team.tsx
+++ src/components/team/team.tsx
@@ -1,7 +1,64 @@
-export function Team() {
+import './team.css'
+import '../../common.css'
+import imagenPrueba from '../../assets/icon_referencia_legal.jpg'
+
+
+
+
+export const Team = () => {
   return (
-    <div>
-      <h1>Team</h1>
-    </div>
+    <>
+      <section id='team' className='team'>
+        <div className='team-content'>
+          <h2 className='section-title'>NUESTRO EQUIPO DE EXPERTOS</h2>
+          <div className='team-members'>
+            <div className='team-member'>
+              <img src={imagenPrueba} alt="MELISA ARBOLEDA OSPINA" />
+              <h3>MELISA ARBOLEDA OSPINA</h3>
+              <p>Directora Jurídica</p>
+            </div>
+            <div className='team-member'>
+              <img src={imagenPrueba} alt="CAROLINA RESTREPO" />
+              <h3>CAROLINA RESTREPO</h3>
+              <p>Abogada Especialista</p>
+            </div>
+            <div className='team-member'>
+              <img src={imagenPrueba} alt="SANTIAGO CARDONA" />
+              <h3>SANTIAGO CARDONA</h3>
+              <p>Auxiliar Jurídico</p>
+            </div>
+            <div className='team-member'>
+              <img src={imagenPrueba} alt="CRISTIAN CASTRO" />
+              <h3>CRISTIAN CASTRO</h3>
+              <p>Director Administrativo y comercial</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id='principles' className='principles'>
+        <div className='principles-content'>
+          <h2 className='section-title'>PRINCIPIOS Y VALORES</h2>
+          <div className='principles-cards'>
+            <div className='principle-card'>
+              <h3>Comunicación y transparencia</h3>
+              <p>En Referencia Legal mantenemos una comunicación constante, asegurando que comprendas cada etapa de tu proceso.</p>
+            </div>
+            <div className='principle-card'>
+              <h3>Compromiso</h3>
+              <p>Nuestro compromiso es tu bienestar, siempre priorizando tus necesidades e intereses en cada acción.</p>
+            </div>
+            <div className='principle-card'>
+              <h3>Criterio ético y moral</h3>
+              <p>Seleccionamos cada caso con responsabilidad ética, ofreciendo un servicio honesto, justo y respetuoso de la ley.</p>
+            </div>
+            <div className='principle-card'>
+              <h3>Procesos humanizados</h3>
+              <p>Humanizamos cada proceso, asegurando claridad en cada decisión y priorizando siempre tu bienestar.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
