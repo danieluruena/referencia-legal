693da76d8da85081d6d1a6d74f0e4868ec789bda
Daniel Urueña <daniel.uruena@hotmail.com>
2026-06-17 14:49:12 -0500

    home first part

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,28 @@
+import './home.css'
+import '../../common.css'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="home-container">
+        <div className="home-hero max-width">
+          <div className="home-description">
+            <h1 className="home-title">Especialistas en Derecho de Familia con enfoque estratégico y humano.</h1>
+            <hr />
+            <button className="main-button">Agendar consulta</button>
+          </div>
+          <div className="image-container">
+            <img src="./src/assets/home/001.webp" alt="" />
+          </div>
+        </div>
+        <hr />
+        <div className="slogan max-width">
+          <div className="slogan-icon">
+            <img src="./src/assets/icon.webp" />
+          </div>
+          <p>Transformamos el Derecho de Familia en un proceso claro y cercano.</p>
+        </div>
+      </div>
     </div>
   )
 }
