759fb1311d6cb0c050ba7b28c1aac0ad0e5aadaf
Brayan Zarabanda <brayan.zarabanda.g@gmail.com>
2026-06-18 20:58:10 -0500

    feat/img second part

--- src/components/team/team.tsx
+++ src/components/team/team.tsx
@@ -5,6 +5,7 @@ import melisaImg from '../../assets/team/Melisa.webp'
 import carolinaImg from '../../assets/team/Carolina.webp'
 import santiagoImg from '../../assets/team/Santiago.webp'
 import cristianImg from '../../assets/team/Cristian.webp'
+import icon from '../../assets/icon.webp'
 
 
 export const Team: FC = () => {
@@ -43,21 +44,23 @@ export const Team: FC = () => {
           <h2 className='section-title'>PRINCIPIOS Y VALORES</h2>
           <div className='principles-cards'>
             <div className='principle-card'>
-              <h3>Comunicación y transparencia</h3>
-              <p>En Referencia Legal, mantenemos una comunicación constante, asegurando que comprendas cada etapa de tu proceso.</p>
+              <h3>Comunicación y transparencia</h3> 
+              <img src={icon} alt="Check" />             
+              <p>Mantenemos una comunicación constante, asegurando que comprendas cada etapa de tu proceso.</p>
             </div>
             <div className='principle-card'>
-
               <h3>Compromiso</h3>
-              <br />
+              <img src={icon} alt="Check" />
               <p>Nuestro compromiso es tu bienestar, siempre priorizando tus necesidades e intereses en cada acción.</p>
             </div>
             <div className='principle-card'>
               <h3>Criterio ético y moral</h3>
+              <img src={icon} alt="Check" />
               <p>Seleccionamos cada caso con responsabilidad ética, ofreciendo un servicio honesto, justo y respetuoso de la ley.</p>
             </div>
             <div className='principle-card'>
               <h3>Procesos humanizados</h3>
+              <img src={icon} alt="Check" />
               <p>Humanizamos cada proceso, asegurando claridad en cada decisión y priorizando siempre tu bienestar.</p>
             </div>
           </div>
